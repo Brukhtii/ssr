4dc420a5b2be68013d9683805bb9297db8f1aebf
Serhii Brukhtii <sergey.brukhtii@gmail.com>
2024-06-09 00:36:10 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ComponentsBlock } from './components/ComponentsBlock';
 
-
-// const componentsBlock = await axios.post('https://hidden-refuge-97632-934ef58eadcb.herokuapp.com/getSSR', {componentsQty: 10})
-//   .then(({data}) => data);
-
 function App() {
-  // const [components, setComponents] = useState('');
-
   return (
     <div className="app">
       <h1>Web page to test SSR in SEO</h1>
